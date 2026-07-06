9cf5cc6a5a67c742b6d2ea7f834ab2efe3c69bee
ShirakawaMio <abcgfsmile@gmail.com>
2026-07-06 15:48:06 +0200

    remove htop conf

--- home/modules/xdg-configs.nix
+++ home/modules/xdg-configs.nix
@@ -5,7 +5,6 @@
 
   xdg.configFile = {
     "ghostty/config".source = ../files/ghostty/config.ghostty;
-    "htop/htoprc".source = ../files/htop/htoprc;
     "karabiner".source = ../files/karabiner;
     "nvim".source = ../files/nvim;
   };
